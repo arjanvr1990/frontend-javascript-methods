f65c4641536b1680c94f6cd6f39b5040e4a3a901
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2023-07-20 13:02:52 +0200

    opdracht date methods af

--- date-methods.js
+++ date-methods.js
@@ -2,8 +2,8 @@
 // Maak een nieuw date-object aan voor dit huidige moment en log dit in de terminal
 // ---- Verwachte uitkomst: 2021-04-06T14:54:34.062Z (afhankelijk van het moment)
 
-const currentDay = new Date();
-console.log(currentDay);
+const currentDay1 = new Date();
+console.log(currentDay1);
 
 
 /* Opdracht 1 */
@@ -13,8 +13,28 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
-
+const currentTimaAndDate = new Date();
+console.log(currentTimaAndDate.getHours());
+console.log( currentTimaAndDate.getMinutes());
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const currentHour = currentTimaAndDate.getHours();
+const currentMinut = currentTimaAndDate.getMinutes();
+const currentYear = currentTimaAndDate.getFullYear();
+
+const currentMonthNumber = currentTimaAndDate.getMonth();
+const currentMonth = monthsOfTheYear[currentMonthNumber];
+
+const currentDayNumber = currentTimaAndDate.getDay();
+const currentDay = dayOfTheWeek[currentDayNumber];
+
+
+console.log(currentHour + " : " + currentMinut + " " + currentYear + " " + currentMonth + " " + currentDay);
+
+console.log("Het is vandaag " + currentDay + " van de maand " + currentMonth + " en het jaar " + currentYear + " de tijd is " + currentHour + " : " + currentMinut);
